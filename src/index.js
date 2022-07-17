8e9048f09f661d2c99044476c4a402eaa601157c
hubertmerk <86593169+Hubert-Merkouloff@users.noreply.github.com>
2022-07-17 22:37:16 +0200

    responsive + async image

--- src/index.js
+++ src/index.js
@@ -1,4 +1,5 @@
 import './style.css';
+import img from './assets/images/580b57fcd9996e24bc43c516.png'
 
 const canvas = document.querySelector('canvas');
 const ctx = canvas.getContext('2d');
@@ -16,8 +17,28 @@ CanvasRenderingContext2D.prototype.roundRect = function (x, y, w, h, r) {
   return this;
 }
 
-let speed = 800;
-let score = 0;
+// variable declaration;
+
+let direction = 'e'
+let gridElem //the grid element depends on the size of the window
+let c = 800
+let score = 0
+let level = 0
+let running = false
+let apple;
+let snake;
+let appleImage;
+
+
+const restoreInitialValues = () => {
+  score = 0;
+  level = 0
+  c = 800;
+  running = false
+  direction = 'e'
+  snake =[[9, 9],[8, 9],[7, 9]]
+  apple = [5,5]
+}
 
 
 document.addEventListener('keydown', (event) => {
@@ -46,34 +67,77 @@ document.addEventListener('keydown', (event) => {
 
 })
 
-let direction = 'e'
-const gridElem = 40; //there is 20 position per axe 800/40
-const snake = [[9, 9],[8, 9],[7, 9]]
-let apple = [5,5]
+window.addEventListener("click", () => {
+  if (!running) {
+    restoreInitialValues()
+    requestAnimationFrame(move);
+  }
+});
 
 
 const drawMap = () => {
-  ctx.fillStyle = 'black';
-  ctx.fillRect(0,0,800,800)
+  ctx.fillStyle = 'white'
+  ctx.fillRect(0,0,canvas.width, canvas.height)
+  ctx.fillStyle = '#ecf0f1';
+  let numberOfElementsByRow = canvas.width / gridElem
+  let numberOfElements = numberOfElementsByRow**2;
+  let rowIndex = 0
+  let columnIndex = 0
+
+  for (let index = 0; index < numberOfElements; index++) {
+    if ( index >0 && index % numberOfElementsByRow === 0) {
+      rowIndex++;
+      columnIndex = 0;
+    }
+    ctx.fillRect( columnIndex * gridElem + 1, rowIndex * gridElem + 1, gridElem -2, gridElem -2 )
+    columnIndex++
+
+  }
+
 };
 
+
+
 const drawSnake = () => {
-  ctx.fillStyle = "green"
+  ctx.fillStyle = "#2ecc71"
   for (let body of snake){
     ctx.fillRect(body[0]*gridElem, body[1]*gridElem, gridElem, gridElem)
 
   }
 }
 
+const getTheApple = async () => {
+
+  try {
+    const result = await new Promise((resolve) => {
+      const image = new Image();
+      image.src = img;
+      image.onload = () => {
+        resolve(image)
+      }
+    })
+    return result;
+  } catch(e) {
+    console.log("error : ", e)
+  }
+
+
+
+
+
+}
+
 const drawApple = () => {
-  ctx.fillStyle = 'red'
-  ctx.fillRect(apple[0]*gridElem, apple[1]*gridElem, gridElem, gridElem)
+  ctx.drawImage(appleImage, apple[0]*gridElem, apple[0]*gridElem, gridElem, gridElem )
 }
 
 const gameOver = () => {
-  if ((snake[0][0] > 19)|| (snake[0][0]<0) || (snake[0][1] < 0 ) || (snake[0][1]>19)){ //if out of the window
+  if ((snake[0][0] > canvas.width / gridElem - 1)|| (snake[0][0]<0) || (snake[0][1] < 0 ) || (snake[0][1]> canvas.height / gridElem - 1)){ //if out of the window
     return true;
-  } else {
+  } else if ((snake[0][0] < 2)&&(snake[0][1] < 2)) {
+    return true;
+  }
+  else {
     const [head, ...body] = snake;
     for (let bodyElement of body) {
       if (bodyElement[0]===head[0] && bodyElement[1]===head[1]){ //if It eats itself
@@ -92,7 +156,11 @@ const eatApple = () => {
 
 const moveApple = () => {
   score++
-  speed = speed + 10
+  if (score!=0 && score%10 === 0) {
+    level++;
+    c = c + 5;
+  }
+
   apple[0] = Math.round(Math.random()*19)
   apple[1] = Math.round(Math.random()*19)
 
@@ -105,57 +173,29 @@ const moveApple = () => {
 }
 
 const drawScore = () => {
+
+  ctx.fillStyle = '#d35400'
+  ctx.fillRect(0, 0,5*gridElem,3*gridElem)
   ctx.fillStyle = "white"
   ctx.font = '40px sans-serif'
-  ctx.textBaseline = 'bottom'
-  ctx.fillText(score,gridElem, gridElem)
+  ctx.textBaseline = 'middle'
+  ctx.textAlign = 'left'
+  ctx.fillText(`Score: ${score}`,gridElem, gridElem)
+  ctx.fillText(`Level: ${level}`,gridElem, 2*gridElem)
   console.log("ctx", ctx)
 }
 
-const drawSpeed = () => {
-  ctx.fillStyle = "white"
-  ctx.font = '40px sans-serif'
-  ctx.textBaseline = 'bottom'
-  ctx.fillText(`${speed} km/h`,gridElem, 3 * gridElem)
-}
+
+
 
 const endTheGame = () => {
-  ctx.clearRect(0,0,800,800);
-  drawMap()
+  ctx.clearRect(0,0,canvas.width,canvas.height);
   ctx.fillStyle = "red"
   ctx.font = '80px sans-serif'
   ctx.textBaseline = 'middle'
   ctx.textAlign = 'center'
-  ctx.fillText('game over', gridElem * 10, gridElem * 10)
+  ctx.fillText('Game over', canvas.width/2, canvas.height/2)
 
-
-  let step = 1;
-
-  const animationGameOver = (timeStamp) => {
-    ctx.save();
-    if (step < 16) {
-      ctx.translate(10,10);
-      ctx.roundRect(20,20,100,100,50);
-      ctx.fill();
-    } else if (step == 16) {
-      ctx.translate(-160,0)
-    } else if (step < 32) {
-      ctx.translate(10,-10)
-      ctx.roundRect(20,20,100,100,50);
-      ctx.fill();
-    }
-
-    step++
-    console.log("step",step)
-    if (step < 100) {
-      //setTimeout(()=> requestAnimationFrame(animationGameOver), 500 )
-      requestAnimationFrame(animationGameOver)
-    }
-
-
-  }
-
-  requestAnimationFrame(animationGameOver)
 
 }
 
@@ -199,19 +239,58 @@ const move = ()=> {
 if (!updateSnakePosition()){
   drawMap();
   drawScore();
-  drawSpeed();
   drawSnake();
   drawApple();
-  setTimeout(()=> requestAnimationFrame(move), 1000-speed )
+  setTimeout(()=> requestAnimationFrame(move), 1000-c )
 
 } else {
-
   endTheGame();
-}
-
 
 }
 
 
+}
 
-requestAnimationFrame(move);
+const start = (image) => {
+  ctx.clearRect(0, 0, canvas.width, canvas.height);
+  appleImage = image;
+  ctx.font = "40px sans-serif";
+  ctx.fillStyle = "black";
+  ctx.textBaseline = "middle";
+  ctx.textAlign = "center";
+  ctx.fillText(
+    "Click to start the game",
+    canvas.width / 2,
+    canvas.height / 2
+  );
+  ctx.drawImage(appleImage, canvas.width/2 - 150/2, canvas.height/2 + 20, 150, 150 )
+
+
+};
+
+
+window.addEventListener("resize", () => {
+  resizeGame();
+});
+
+const resizeGame = () => {
+  if (window.innerWidth < 576) {
+    gridElem = 20;
+  } else if (window.innerWidth < 768) {
+    gridElem = 25;
+  } else if (window.innerWidth < 992) {
+    gridElem = 30;
+  } else if (window.innerWidth < 992) {
+    gridElem = 35;
+  } else if (window.innerWidth < 1200) {
+    gridElem = 40;
+  } else if (window.innerWidth >= 1200) {
+    gridElem = 45;
+  }
+  canvas.width = Math.floor((window.innerWidth - 2) / gridElem) * gridElem;
+  canvas.height = Math.floor((window.innerHeight - 2) / gridElem) * gridElem;
+
+  getTheApple().then((image) => start(image))
+};
+
+resizeGame();
